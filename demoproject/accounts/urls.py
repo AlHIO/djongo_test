cdef80013a3416a1f44724cc078ecf082bcb5248
AlHIO <alalthegamegun.c@nycu.edu.tw>
2025-04-29 20:13:56 +0800

    account to LoginAPIView vesion

--- demoproject/accounts/urls.py
+++ demoproject/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import LoginView, LogoutView
+from .views import LoginAPIView, LogoutAPIView
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', LoginAPIView.as_view(), name='api-login'),
++   path('logout/', LogoutAPIView.as_view(), name='api-logout'),
 ]
