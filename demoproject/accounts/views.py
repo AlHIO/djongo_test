6a33d0956f896e9082f46f73dbea6f3cd2a68cbc
AlHIO <alalthegamegun.c@nycu.edu.tw>
2025-04-29 19:33:01 +0800

    adopt accounts/views.py

--- demoproject/accounts/views.py
+++ demoproject/accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView as DjangoLoginView, LogoutView as DjangoLogoutView
+from django.urls import reverse_lazy
 
-# Create your views here.
+class LoginView(DjangoLoginView):
+    template_name = 'registration/login.html'   # 可先用 Django 預設模板
+    success_url = reverse_lazy('some_dashboard')  # 登入後導向
+
+class LogoutView(DjangoLogoutView):
+    next_page = reverse_lazy('login')            # 登出後導向
