3e41da3870829dc75c5013c5ea8e4071312476f2
AlHIO <alalthegamegun.c@nycu.edu.tw>
2025-04-29 21:37:42 +0800

    demo/urls.py add profileview

--- demoproject/demoproject/urls.py
+++ demoproject/demoproject/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from accounts.views import SignUpView
+from accounts.views import SignUpView, ProfileView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +10,9 @@ urlpatterns = [
 
     # HTML 登入/登出
     path('accounts/', include('django.contrib.auth.urls')),
+
+    # 使用者個人頁面（登入成功後 redirect 會到這）
+    path('accounts/profile/', ProfileView.as_view(), name='profile'),
 
     # JSON API 登入/登出
     path('api/accounts/', include('accounts.api_urls')),
